b225f71fe30b235f5745983887465fc1249118e6
Dmitry Gladkov <dgladkov1709@gmail.com>
2017-02-18 22:24:09 +0300

    GeneralizationFile class has been updated fully.
    Signed-off-by: Dmitry Gladkov <dgladkov1709@gmail.com>

--- Generalization_Server/GenaraliztionFile.cpp
+++ Generalization_Server/GenaraliztionFile.cpp
@@ -1,4 +1,6 @@
 #include "GeneralizationFile.h"
+#include <cassert>
+#include <iostream>
 
 GenaraliztionFile::GenaraliztionFile()
 {
@@ -11,18 +13,54 @@ void GenaraliztionFile::SetPath(std::string string_path)
 	path = string_path;
 }
 
+uint32_t GenaraliztionFile::GetNumberOfCurves()
+{
+	return numOfCurves;
+}
+
+curves GenaraliztionFile::GetCurves()
+{
+	return Curves;
+}
+
+curveCoord GenaraliztionFile::GetCurveCoordPointsX(uint32_t curve)
+{
+	assert(curve < numOfCurves);
+
+	return curvesX[curve];
+}
+
+curveCoord GenaraliztionFile::GetCurveCoordPointsY(uint32_t curve)
+{
+	assert(curve < numOfCurves);
+
+	return curvesY[curve];
+}
+
+uint32_t GenaraliztionFile::GetCurveNumberPoints(uint32_t curve)
+{
+	assert(curve < numOfCurves);
+
+	return pointInCurves[curve];
+}
+
 void GenaraliztionFile::ParseAllDataInFile()
 {
 	char separator = ' ';
 
 	System::String ^ gcPath = gcnew System::String(path.c_str());
 	array<System::String^>^ lines = System::IO::File::ReadAllLines(gcPath);
-	numOfCurves = System::Convert::ToUInt64(lines[0]);
+	numOfCurves = System::Convert::ToUInt32(lines[0]);
 
 	int k = -1;
 	int X = 0;
 	int Y = 0;
 	bool first = false;
+
+	pointInCurves.resize(numOfCurves);
+
+	curvesX.resize(numOfCurves);
+	curvesY.resize(numOfCurves);
 
 	for each (System::String^ str in lines)
 	{
@@ -36,40 +74,52 @@ void GenaraliztionFile::ParseAllDataInFile()
 			X = 0;
 			Y = 0;
 			k++;
-			pointInCurves[k] = System::Convert::ToUInt64(str);
 
-			//System.Windows.Forms.MessageBox.Show(str);
+			pointInCurves[k] = System::Convert::ToUInt32(str);
+
+			curvesX[k].resize(pointInCurves[k]);
+			curvesY[k].resize(pointInCurves[k]);
 		}
 		else if (str[0] == 'M')
 		{
 			array<System::String^> ^rawCord = str->Split(separator);
-			int AuxK = 3;
-			std::string AuxStr = "";
-			//System.Windows.Forms.MessageBox.Show(str);
+			uint32_t AuxK = 3;
+			System::String^ AuxStr = "";
+
 			while (rawCord[0][AuxK] != ')')
 			{
 				AuxStr += rawCord[0][AuxK];
 				AuxK++;
 			}
 
-			uint64_t count = System::Convert::ToUInt64(AuxStr.c_str()) + 5 < pointInCurves[k] ?
-				5 * 2 + 1 : (pointInCurves[k] - System::Convert::ToUInt64(AuxStr.c_str()) + 1) * 2 + 1;
+			uint32_t count = (System::Convert::ToUInt32(AuxStr) + 5 < pointInCurves[k]) ?
+				(5 * 2 + 1) : ((pointInCurves[k] - System::Convert::ToUInt32(AuxStr) + 1) * 2 + 1);
 
-			for (uint64_t i = 1; i < count; i++)
+			for (uint32_t i = 1; i < count; i++)
 			{
 				if (i % 2 != 0)
 				{
-					curvesX[k][X] = System::Convert::ToInt64(rawCord[i]);
+					curvesX[k][X] = System::Convert::ToDouble(rawCord[i]);
 					X++;
 				}
 				else
 				{
-					curvesY[k][Y] = System::Convert::ToInt64(rawCord[i]);
-					//System.Windows.Forms.MessageBox.Show(CurvesY[k][Y].ToString());
+					curvesY[k][Y] = System::Convert::ToDouble(rawCord[i]);
 					Y++;
 				}
 			}
 		}
+	}
+
+	BuildCurvesMap();
+}
+
+void GenaraliztionFile::BuildCurvesMap()
+{
+	for (uint32_t i = 0; i < numOfCurves; i++)
+	{
+		Curves[i].first = curvesX[i];
+		Curves[i].second = curvesY[i];
 	}
 }
 
--- Generalization_Server/GeneralizationFile.h
+++ Generalization_Server/GeneralizationFile.h
@@ -3,18 +3,33 @@
 #include <string>
 #include <list>
 #include <vector>
+#include <utility>
+#include <map>
+
+typedef std::vector<double_t> curveCoord;
+typedef std::vector<std::vector<double_t>> curvesCoord;
+typedef std::pair<curveCoord, curveCoord> curve;
+typedef std::map<uint32_t, curve> curves;
 
 class GenaraliztionFile
 {
 private:
 	std::string path;
 	std::string pathRes;
-	uint64_t numOfCurves = 0;
-	std::vector<uint64_t> pointInCurves;
-	std::vector<std::vector<double_t>> curvesX;
-	std::vector<std::vector<double_t>> curvesY;
+	uint32_t numOfCurves = 0;
+	std::vector<uint32_t> pointInCurves;
+	curvesCoord curvesX;
+	curvesCoord curvesY;
+	curves Curves;
+
+	void BuildCurvesMap();
 public:
 	GenaraliztionFile();
+	uint32_t GetNumberOfCurves();
+	curves GetCurves();
+	curveCoord GetCurveCoordPointsX(uint32_t curve);
+	curveCoord GetCurveCoordPointsY(uint32_t curve);
+	uint32_t GetCurveNumberPoints(uint32_t curve);
 	void SetPath(std::string string_path);
 	void ParseAllDataInFile();
 	virtual ~GenaraliztionFile();
